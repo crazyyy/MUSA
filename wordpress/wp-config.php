0f2117396ee067d8d6a8dad5580a918b8f92ae40
Vitalii Antoniuk <va@jelastic.com>
2019-09-11 13:14:30 +0300

    wp ready

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_NAME', 'DBNAME');
-define('DB_USER', 'DBUSER');
-define('DB_PASSWORD', 'DBPASS');
+define('DB_NAME', 'musa');
+define('DB_USER', 'musa');
+define('DB_PASSWORD', 'musa');
 
 define('DB_HOST', 'localhost');
 
